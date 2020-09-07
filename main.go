b81e50fca50388932e62f2d8e5e64b81b27ad53c
RafaelOFreitas <rafa.oliveirafreitas@hotmail.com>
2020-09-07 14:56:06 -0300

    Criando tela de Status

--- main.go
+++ main.go
@@ -21,13 +21,13 @@ func main() {
 	presentation, windowsGenerateionError := window.New(sciter.SW_TITLEBAR|sciter.SW_TOOL|sciter.SW_CONTROLS, windowRect)
 
 	if windowsGenerateionError != nil {
-		log.Println("Failed to generate sciter window ", windowsGenerateionError.Error())
+		log.Println("Failed to generate sciter window Presentation", windowsGenerateionError.Error())
 	}
 
 	uiLoadingError := presentation.LoadFile("./ui/presentation/index.html")
 
 	if uiLoadingError != nil {
-		log.Println("Failed to load ui file ", uiLoadingError.Error())
+		log.Println("Failed to load ui file ./ui/presentation/index.html ", uiLoadingError.Error())
 	}
 
 	showSplashScreen(presentation)
@@ -49,13 +49,13 @@ func showSplashScreen(mainScreen *window.Window) {
 	splashScreen, windowsGenerateionError := window.New(sciter.SW_TOOL, rect)
 
 	if windowsGenerateionError != nil {
-		log.Println("Failed to generate sciter window ", windowsGenerateionError.Error())
+		log.Println("Failed to generate sciter window SplashScreen", windowsGenerateionError.Error())
 	}
 
 	uiLoadingError := splashScreen.LoadFile("./ui/splash-screen/index.html")
 
 	if uiLoadingError != nil {
-		log.Println("Failed to load ui file ", uiLoadingError.Error())
+		log.Println("Failed to load ui file ./ui/splash-screen/index.html ", uiLoadingError.Error())
 	}
 
 	splashScreen.DefineFunction("load_main_screen", func(...*sciter.Value) *sciter.Value {
@@ -77,13 +77,13 @@ func showToolSelection(key *sciter.Value) {
 	toolSelection, windowsGenerateionError := window.New(sciter.SW_TITLEBAR|sciter.SW_TOOL|sciter.SW_CONTROLS, rect)
 
 	if windowsGenerateionError != nil {
-		log.Println("Failed to generate sciter window ", windowsGenerateionError.Error())
+		log.Println("Failed to generate sciter window ToolSelection", windowsGenerateionError.Error())
 	}
 
 	uiLoadingError := toolSelection.LoadFile("./ui/tool-selection/index.html")
 
 	if uiLoadingError != nil {
-		log.Println("Failed to load ui file ", uiLoadingError.Error())
+		log.Println("Failed to load ui file ./ui/tool-selection/index.html ", uiLoadingError.Error())
 	}
 
 	toolSelection.Call("selectElement", sciter.NewValue(key))
@@ -97,7 +97,31 @@ func showToolSelection(key *sciter.Value) {
 		return nil
 	})
 
+	toolSelection.DefineFunction("load_status", func(...*sciter.Value) *sciter.Value {
+		showStatus()
+
+		return nil
+	})
+
 	toolSelection.Show()
+}
+
+func showStatus() {
+	rect := sciter.NewRect(250, 500, 635, 625)
+
+	status, windowsGenerateionError := window.New(sciter.SW_TITLEBAR|sciter.SW_TOOL|sciter.SW_CONTROLS, rect)
+
+	if windowsGenerateionError != nil {
+		log.Println("Failed to generate sciter window Status", windowsGenerateionError.Error())
+	}
+
+	uiLoadingError := status.LoadFile("./ui/status/index.html")
+
+	if uiLoadingError != nil {
+		log.Println("Failed to load ui file ./ui/status/index.html ", uiLoadingError.Error())
+	}
+
+	status.Show()
 }
 
 func parseJSON(key *sciter.Value) []byte {
